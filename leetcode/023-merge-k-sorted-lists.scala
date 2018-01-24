bbdff30755d088e38a77816859bd69cebd37d38e
airt <airtial@gmail.com>
2018-01-24 15:02:09 +0800

    feat: leetcode/023-merge-k-sorted-lists

---
+++ leetcode/023-merge-k-sorted-lists.scala
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/merge-k-sorted-lists
+
+/**
+  * class ListNode(
+  *   var x: Int = 0,
+  *   var next: ListNode = null
+  * )
+  */
+
+object Solution {
+
+  def mergeKLists(xss: Array[ListNode]): ListNode =
+    xss reduceOption merge getOrElse null
+
+  def merge(xs: ListNode, ys: ListNode): ListNode = merge(xs, ys, identity)
+
+  def merge(xs: ListNode, ys: ListNode, k: ListNode => ListNode): ListNode =
+    if (xs eq null) k(ys) else
+    if (ys eq null) k(xs) else
+    if (xs.x < ys.x)
+      merge(xs.next, ys, k compose cons(xs.x))
+    else
+      merge(xs, ys.next, k compose cons(ys.x))
+
+  def cons(x: Int) = (xs: ListNode) => {
+    val n = new ListNode(x)
+    n.next = xs
+    n
+  }
+
+}
