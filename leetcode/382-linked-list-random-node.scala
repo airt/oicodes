9b0e068327d4eb7ec3d30c468ab1245164566d7f
airt <airtial@gmail.com>
2018-01-24 15:01:31 +0800

    feat: leetcode/382-linked-list-random-node

--- leetcode/382-linked-list-random-node.scala
+++ leetcode/382-linked-list-random-node.scala
@@ -14,7 +14,7 @@ class Solution(head: ListNode) {
   def getRandom(): Int = {
     def randomOneNth(n: Int): Boolean = (Random nextInt n) == 0
     def h(n: Int, node: ListNode, z: Int): Int =
-      if (node == null) z
+      if (node eq null) z
       else h(n + 1, node.next, if (randomOneNth(n)) node.x else z)
     h(1, head, 0)
   }
