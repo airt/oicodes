dd952d62e469fd643a8ef676ed6401a9052239b8
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/2.add-two-numbers.rs
+++ leetcode/2.add-two-numbers.rs
@@ -4,42 +4,38 @@
  * [2] Add Two Numbers
  */
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+  pub fn new(val: i32) -> Self {
+    ListNode { val, next: None }
+  }
+}
+
+pub struct Solution;
+
 // @lc code=start
-// Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
-// pub struct ListNode {
-//   pub val: i32,
-//   pub next: Option<Box<ListNode>>
-// }
-//
-// impl ListNode {
-//   #[inline]
-//   fn new(val: i32) -> Self {
-//     ListNode {
-//       next: None,
-//       val
-//     }
-//   }
-// }
 impl Solution {
-    pub fn add_two_numbers(
-        x: Option<Box<ListNode>>,
-        y: Option<Box<ListNode>>,
-    ) -> Option<Box<ListNode>> {
-        match (x, y) {
-            (None, None) => None,
-            (None, Some(y)) => Some(y),
-            (Some(x), None) => Some(x),
-            (Some(x), Some(y)) => {
-                let mut v = x.val + y.val;
-                let mut n = Self::add_two_numbers(x.next, y.next);
-                if v >= 10 {
-                    v -= 10;
-                    n = Self::add_two_numbers(n, Some(Box::new(ListNode::new(1))));
-                }
-                Some(Box::new(ListNode { val: v, next: n }))
-            }
+  pub fn add_two_numbers(
+    x: Option<Box<ListNode>>,
+    y: Option<Box<ListNode>>,
+  ) -> Option<Box<ListNode>> {
+    match (x, y) {
+      (Some(x), Some(y)) => {
+        let mut v = x.val + y.val;
+        let mut n = Self::add_two_numbers(x.next, y.next);
+        if v >= 10 {
+          v -= 10;
+          n = Self::add_two_numbers(n, Some(Box::new(ListNode::new(1))));
         }
+        Some(Box::new(ListNode { val: v, next: n }))
+      }
+      (x, y) => x.or(y),
     }
+  }
 }
 // @lc code=end
