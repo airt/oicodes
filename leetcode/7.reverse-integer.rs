cc0c89a139747ad1ba4dff229ba0bdc65d8b8b4c
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/7.reverse-integer.rs
+++ leetcode/7.reverse-integer.rs
@@ -4,19 +4,21 @@
  * [7] Reverse Integer
  */
 
+pub struct Solution;
+
 // @lc code=start
 impl Solution {
-    pub fn reverse(x: i32) -> i32 {
-        let h = || {
-            let mut x = x;
-            let mut z = 0i32;
-            while x != 0 {
-                z = z.checked_mul(10)?.checked_add(x % 10)?;
-                x = x / 10;
-            }
-            Some(z)
-        };
-        h().unwrap_or_default()
-    }
+  pub fn reverse(x: i32) -> i32 {
+    let h = || {
+      let mut x = x;
+      let mut z = 0;
+      while x != 0 {
+        z = 10i32.checked_mul(z)?.checked_add(x % 10)?;
+        x /= 10;
+      }
+      Some(z)
+    };
+    h().unwrap_or_default()
+  }
 }
 // @lc code=end
