d6dffd69a7c26b58ebab1d2ffe1f1cb7e4b56cd4
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode/1.two-sum

--- leetcode/1.two-sum.rs
+++ leetcode/1.two-sum.rs
@@ -9,9 +9,10 @@ impl Solution {
     pub fn two_sum(xs: Vec<i32>, z: i32) -> Vec<i32> {
         use std::collections::HashMap;
         let mut m = HashMap::new();
-        for (i, x) in xs.iter().enumerate() {
+        for (i, x) in xs.into_iter().enumerate() {
+            let i = i as i32;
             if let Some(&j) = m.get(&(z - x)) {
-                return vec![j as i32, i as i32];
+                return vec![j, i];
             }
             m.insert(x, i);
         }
