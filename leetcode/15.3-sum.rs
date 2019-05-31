e2b9da1048931c560a516a9e21bbda1de7f8f993
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/15.3-sum.rs
+++ leetcode/15.3-sum.rs
@@ -4,29 +4,31 @@
  * [15] 3Sum
  */
 
+pub struct Solution;
+
 // @lc code=start
 impl Solution {
-    pub fn three_sum(mut xs: Vec<i32>) -> Vec<Vec<i32>> {
-        use std::collections::HashSet;
-        let mut rs = HashSet::new();
-        xs.sort_unstable();
-        for i in 0..xs.len() {
-            let mut i1 = i + 1;
-            let mut i2 = xs.len() - 1;
-            while i1 < i2 {
-                let r = vec![xs[i], xs[i1], xs[i2]];
-                let s = r.iter().sum::<i32>();
-                if s == 0 {
-                    rs.insert(r);
-                }
-                if s <= 0 {
-                    i1 += 1;
-                } else {
-                    i2 -= 1;
-                }
-            }
+  pub fn three_sum(mut xs: Vec<i32>) -> Vec<Vec<i32>> {
+    use std::collections::HashSet;
+    let mut rs = HashSet::new();
+    xs.sort_unstable();
+    for i in 0..xs.len() {
+      let mut i1 = i + 1;
+      let mut i2 = xs.len() - 1;
+      while i1 < i2 {
+        let r = vec![xs[i], xs[i1], xs[i2]];
+        let s = r.iter().sum::<i32>();
+        if s == 0 {
+          rs.insert(r);
         }
-        rs.into_iter().collect()
+        if s <= 0 {
+          i1 += 1;
+        } else {
+          i2 -= 1;
+        }
+      }
     }
+    rs.into_iter().collect()
+  }
 }
 // @lc code=end
--- leetcode/16.3-sum-closest.rs
+++ leetcode/16.3-sum-closest.rs
@@ -4,27 +4,29 @@
  * [16] 3Sum Closest
  */
 
+pub struct Solution;
+
 // @lc code=start
 impl Solution {
-    pub fn three_sum_closest(mut xs: Vec<i32>, z: i32) -> i32 {
-        let mut r = i32::MAX;
-        xs.sort_unstable();
-        for i in 0..xs.len() {
-            let mut i1 = i + 1;
-            let mut i2 = xs.len() - 1;
-            while i1 < i2 {
-                let s = xs[i] + xs[i1] + xs[i2];
-                if (s - z).abs() < (r - z).abs() {
-                    r = s;
-                }
-                if s <= z {
-                    i1 += 1;
-                } else {
-                    i2 -= 1;
-                }
-            }
+  pub fn three_sum_closest(mut xs: Vec<i32>, z: i32) -> i32 {
+    let mut r = i32::MAX;
+    xs.sort_unstable();
+    for i in 0..xs.len() {
+      let mut i1 = i + 1;
+      let mut i2 = xs.len() - 1;
+      while i1 < i2 {
+        let s = xs[i] + xs[i1] + xs[i2];
+        if (s - z).abs() < (r - z).abs() {
+          r = s;
         }
-        r
+        if s <= z {
+          i1 += 1;
+        } else {
+          i2 -= 1;
+        }
+      }
     }
+    r
+  }
 }
 // @lc code=end
