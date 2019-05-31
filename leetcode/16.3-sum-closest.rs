f1decd223db7934a66d8636407b678a2f3db4df4
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode/16.3-sum-closest

--- leetcode/16.3-sum-closest.rs
+++ leetcode/16.3-sum-closest.rs
@@ -7,28 +7,17 @@
 // @lc code=start
 impl Solution {
     pub fn three_sum_closest(mut xs: Vec<i32>, z: i32) -> i32 {
-        let mut r = std::i32::MAX;
+        let mut r = i32::MAX;
         xs.sort_unstable();
         for i in 0..xs.len() {
-            if i > 0 && xs[i] == xs[i - 1] {
-                continue;
-            }
             let mut i1 = i + 1;
             let mut i2 = xs.len() - 1;
             while i1 < i2 {
-                if i1 > i + 1 && xs[i1] == xs[i1 - 1] {
-                    i1 += 1;
-                    continue;
+                let s = xs[i] + xs[i1] + xs[i2];
+                if (s - z).abs() < (r - z).abs() {
+                    r = s;
                 }
-                if i2 < xs.len() - 1 && xs[i2] == xs[i2 + 1] {
-                    i2 -= 1;
-                    continue;
-                }
-                let m = xs[i] + xs[i1] + xs[i2];
-                if (m - z).abs() < (r - z).abs() {
-                    r = m;
-                }
-                if m <= z {
+                if s <= z {
                     i1 += 1;
                 } else {
                     i2 -= 1;
