af3080ad3911d05408348cc8aa741ad5e3fb5329
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode/3.longest-substring-without-repeating-characters

--- leetcode/3.longest-substring-without-repeating-characters.rs
+++ leetcode/3.longest-substring-without-repeating-characters.rs
@@ -7,12 +7,11 @@
 // @lc code=start
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        use std::collections::hash_map::Entry;
         use std::collections::HashMap;
         let mut m = HashMap::new();
         let mut b = -1;
         let mut l = 0;
-        for (i, c) in s.chars().enumerate() {
+        for (i, c) in s.bytes().enumerate() {
             let i = i as i32;
             if let Some(v) = m.insert(c, i) {
                 b = b.max(v);
