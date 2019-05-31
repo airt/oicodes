bf2b7f3c4c0b22ee463d3541d532ac697d24ffac
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/1.two-sum.rs
+++ leetcode/1.two-sum.rs
@@ -4,19 +4,21 @@
  * [1] Two Sum
  */
 
+pub struct Solution;
+
 // @lc code=start
 impl Solution {
-    pub fn two_sum(xs: Vec<i32>, z: i32) -> Vec<i32> {
-        use std::collections::HashMap;
-        let mut m = HashMap::new();
-        for (i, x) in xs.into_iter().enumerate() {
-            let i = i as i32;
-            if let Some(&j) = m.get(&(z - x)) {
-                return vec![j, i];
-            }
-            m.insert(x, i);
-        }
-        unreachable!();
+  pub fn two_sum(xs: Vec<i32>, z: i32) -> Vec<i32> {
+    use std::collections::HashMap;
+    let mut m = HashMap::new();
+    for (i, x) in xs.into_iter().enumerate() {
+      let i = i as i32;
+      if let Some(&j) = m.get(&(z - x)) {
+        return vec![j, i];
+      }
+      m.insert(x, i);
     }
+    unreachable!();
+  }
 }
 // @lc code=end
