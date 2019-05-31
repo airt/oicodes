d2543bc0209c175de5589394a901c00f8f9ea57e
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/5.longest-palindromic-substring.rs
+++ leetcode/5.longest-palindromic-substring.rs
@@ -4,15 +4,15 @@
  * [5] Longest Palindromic Substring
  */
 
+pub struct Solution;
+
 // @lc code=start
 impl Solution {
-    pub fn longest_palindrome(s: String) -> String {
-        let h = || {
-            let mut c = (0..s.len()).rev().flat_map(|n| s.as_bytes().windows(n + 1));
-            let s = c.find(|s| s.iter().rev().eq(s.iter()))?;
-            String::from_utf8(s.to_vec()).ok()
-        };
-        h().unwrap_or_default()
-    }
+  pub fn longest_palindrome(s: String) -> String {
+    let mut s = (0..s.len()).rev().flat_map(|x| s.as_bytes().windows(x + 1));
+    let s = s.find(|s| s.iter().rev().eq(s.iter()));
+    let s = s.map(|s| s.iter().map(|&x| x as char).collect());
+    s.unwrap_or_default()
+  }
 }
 // @lc code=end
