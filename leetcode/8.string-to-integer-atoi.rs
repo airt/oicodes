6ca80da0d94b76cbe5ba5db5ba764fff52180ba2
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/8.string-to-integer-atoi.rs
+++ leetcode/8.string-to-integer-atoi.rs
@@ -4,20 +4,21 @@
  * [8] String to Integer (atoi)
  */
 
+pub struct Solution;
+
 // @lc code=start
 impl Solution {
-    pub fn my_atoi(s: String) -> i32 {
-        let mut s = s.trim_start().bytes().peekable();
-        let p = s
-            .peek()
-            .cloned()
-            .filter(|b| [0x2b, 0x2d].contains(b))
-            .and_then(|_| s.next())
-            .map_or(1, |b| 0x2c - b as i32);
-        s.take_while(|b| (0x30..0x3a).contains(b))
-            .try_fold(0i32, |z, b| z.checked_mul(10)?.checked_add(b as i32 - 0x30))
-            .and_then(|n| n.checked_mul(p))
-            .unwrap_or(if p < 0 { std::i32::MIN } else { std::i32::MAX })
-    }
+  pub fn my_atoi(s: String) -> i32 {
+    let b = |x| [b'+', b'-'].contains(&x).then(|| b',' as i32 - x as i32);
+    let d = |x| (b'0'..b':').contains(&x).then(|| x as i32 - b'0' as i32);
+    let f = |z, x| 10i32.checked_mul(z)?.checked_add(x);
+    let s = s.trim_start();
+    let p = s.bytes().next().and_then(b);
+    let s = s.bytes().skip(p.map_or(0, |_| 1));
+    let p = p.unwrap_or(1);
+    let n = s.map_while(d).try_fold(0, f);
+    let n = n.and_then(|n| n.checked_mul(p));
+    n.unwrap_or(if p < 0 { i32::MIN } else { i32::MAX })
+  }
 }
 // @lc code=end
