c05afd29bd52f7e00e2c3ac923931b47e980819e
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/10.regular-expression-matching.rs
+++ leetcode/10.regular-expression-matching.rs
@@ -4,24 +4,21 @@
  * [10] Regular Expression Matching
  */
 
+pub struct Solution;
+
 // @lc code=start
 impl Solution {
-    pub fn is_match(s: String, p: String) -> bool {
-        fn h(s: &[u8], p: &[u8]) -> bool {
-            if p.is_empty() {
-                s.is_empty()
-            } else {
-                let match_single = !s.is_empty() && (p[0] == s[0] || p[0] == b'.');
-                let has_wildcard = p.len() > 1 && p[1] == b'*';
-                match (match_single, has_wildcard) {
-                    (true, true) => h(&s[1..], p) || h(s, &p[2..]),
-                    (true, false) => h(&s[1..], &p[1..]),
-                    (false, true) => h(s, &p[2..]),
-                    (false, false) => false,
-                }
-            }
-        }
-        h(s.as_bytes(), p.as_bytes())
+  pub fn is_match(s: String, p: String) -> bool {
+    fn h(s: &[u8], p: &[u8]) -> bool {
+      match (s, p) {
+        ([], []) => true,
+        ([x, ..], [z, b'*', ..]) if x == z || z == &b'.' => h(&s[1..], p) || h(s, &p[2..]),
+        ([x, ..], [z, ..]) if x == z || z == &b'.' => h(&s[1..], &p[1..]),
+        ([..], [_, b'*', ..]) => h(s, &p[2..]),
+        ([..], [..]) => false,
+      }
     }
+    h(s.as_bytes(), p.as_bytes())
+  }
 }
 // @lc code=end
