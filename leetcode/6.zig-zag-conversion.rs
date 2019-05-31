a01d4c1121f795910d4e3b218daa653c9f44e409
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/6.zig-zag-conversion.rs
+++ leetcode/6.zig-zag-conversion.rs
@@ -4,13 +4,15 @@
  * [6] ZigZag Conversion
  */
 
+pub struct Solution;
+
 // @lc code=start
 impl Solution {
-    pub fn convert(s: String, n: i32) -> String {
-        let xs = (0..n).chain((1..n - 1).rev()).cycle();
-        let mut zs = xs.zip(s.chars()).collect::<Vec<_>>();
-        zs.sort_by_key(|t| t.0);
-        zs.into_iter().map(|t| t.1).collect()
-    }
+  pub fn convert(s: String, n: i32) -> String {
+    let xs = (0..n).chain((1..n - 1).rev()).cycle();
+    let mut xs = xs.zip(s.bytes()).collect::<Vec<_>>();
+    xs.sort_by_key(|&(i, _)| i);
+    xs.into_iter().map(|(_, x)| x as char).collect()
+  }
 }
 // @lc code=end
