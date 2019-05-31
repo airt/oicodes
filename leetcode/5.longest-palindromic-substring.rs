601c570c88cbf0779fc583af68f9b3b1c1b9e2bd
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode/5.longest-palindromic-substring

--- leetcode/5.longest-palindromic-substring.rs
+++ leetcode/5.longest-palindromic-substring.rs
@@ -7,13 +7,12 @@
 // @lc code=start
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
-        (0..s.len())
-            .rev()
-            .flat_map(|n| s.as_bytes().windows(n + 1))
-            .filter(|s| s.iter().rev().eq(s.iter()))
-            .next()
-            .and_then(|s| String::from_utf8(s.to_vec()).ok())
-            .unwrap_or_default()
+        let h = || {
+            let mut c = (0..s.len()).rev().flat_map(|n| s.as_bytes().windows(n + 1));
+            let s = c.find(|s| s.iter().rev().eq(s.iter()))?;
+            String::from_utf8(s.to_vec()).ok()
+        };
+        h().unwrap_or_default()
     }
 }
 // @lc code=end
