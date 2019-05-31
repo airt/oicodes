a9a570ef5515efe211975ff79af99394d24ec0d7
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/12.integer-to-roman.rs
+++ leetcode/12.integer-to-roman.rs
@@ -4,21 +4,24 @@
  * [12] Integer to Roman
  */
 
+pub struct Solution;
+
 // @lc code=start
 impl Solution {
-    pub fn int_to_roman(n: i32) -> String {
-        let m = [
-            "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "", "X", "XX", "XXX", "XL",
-            "L", "LX", "LXX", "LXXX", "XC", "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC",
-            "CM", "", "M", "MM", "MMM",
-        ];
-        let r = [
-            m[n as usize % 10000 / 1000 + 30],
-            m[n as usize % 1000 / 100 + 20],
-            m[n as usize % 100 / 10 + 10],
-            m[n as usize % 10],
-        ];
-        r.concat()
-    }
+  pub fn int_to_roman(n: i32) -> String {
+    let m = [
+      "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "", "X", "XX", "XXX", "XL", "L",
+      "LX", "LXX", "LXXX", "XC", "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "",
+      "M", "MM", "MMM",
+    ];
+    let n = n as usize;
+    let r = [
+      m[n % 10000 / 1000 + 30],
+      m[n % 1000 / 100 + 20],
+      m[n % 100 / 10 + 10],
+      m[n % 10],
+    ];
+    r.concat()
+  }
 }
 // @lc code=end
