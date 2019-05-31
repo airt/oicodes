040959d36c9f11bd278af70d2c498e2dab874009
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/21.merge-two-sorted-lists.rs
+++ leetcode/21.merge-two-sorted-lists.rs
@@ -4,42 +4,38 @@
  * [21] Merge Two Sorted Lists
  */
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+  pub fn new(val: i32) -> Self {
+    ListNode { val, next: None }
+  }
+}
+
+pub struct Solution;
+
 // @lc code=start
-// Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
-// pub struct ListNode {
-//   pub val: i32,
-//   pub next: Option<Box<ListNode>>
-// }
-//
-// impl ListNode {
-//   #[inline]
-//   fn new(val: i32) -> Self {
-//     ListNode {
-//       next: None,
-//       val
-//     }
-//   }
-// }
 impl Solution {
-    pub fn merge_two_lists(
-        x: Option<Box<ListNode>>,
-        y: Option<Box<ListNode>>,
-    ) -> Option<Box<ListNode>> {
-        match (x, y) {
-            (None, None) => None,
-            (None, Some(y)) => Some(y),
-            (Some(x), None) => Some(x),
-            (Some(mut x), Some(mut y)) => {
-                let (x, y, mut h) = if x.val < y.val {
-                    (x.next.take(), Some(y), x)
-                } else {
-                    (Some(x), y.next.take(), y)
-                };
-                h.next = Self::merge_two_lists(x, y);
-                Some(h)
-            }
-        }
+  pub fn merge_two_lists(
+    x: Option<Box<ListNode>>,
+    y: Option<Box<ListNode>>,
+  ) -> Option<Box<ListNode>> {
+    match (x, y) {
+      (Some(mut x), Some(mut y)) => {
+        let (x, y, mut z) = if x.val <= y.val {
+          (x.next.take(), Some(y), x)
+        } else {
+          (Some(x), y.next.take(), y)
+        };
+        z.next = Self::merge_two_lists(x, y);
+        Some(z)
+      }
+      (x, y) => x.or(y),
     }
+  }
 }
 // @lc code=end
