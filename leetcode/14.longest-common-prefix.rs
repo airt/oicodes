563dd8302c6219cd7ee2b3bf9c8632dba24e7ce1
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/14.longest-common-prefix.rs
+++ leetcode/14.longest-common-prefix.rs
@@ -4,17 +4,16 @@
  * [14] Longest Common Prefix
  */
 
+pub struct Solution;
+
 // @lc code=start
 impl Solution {
-    pub fn longest_common_prefix(ss: Vec<String>) -> String {
-        ss.iter()
-            .fold(ss.first().cloned().unwrap_or_default(), |z, x| {
-                z.chars()
-                    .zip(x.chars())
-                    .take_while(|(z, x)| z == x)
-                    .map(|(z, _)| z)
-                    .collect()
-            })
-    }
+  pub fn longest_common_prefix(s: Vec<String>) -> String {
+    let s = s.into_iter().reduce(|x, y| {
+      let z = x.bytes().zip(y.bytes()).take_while(|(x, y)| x == y);
+      z.map(|(x, _)| x as char).collect()
+    });
+    s.unwrap_or_default()
+  }
 }
 // @lc code=end
