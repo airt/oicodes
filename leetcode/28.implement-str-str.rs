4481a233eb140d022b56d2729bd9ae01f0f4d53b
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/28.implement-str-str.rs
+++ leetcode/28.implement-str-str.rs
@@ -9,13 +9,13 @@ pub struct Solution;
 // @lc code=start
 impl Solution {
   pub fn str_str(x: String, y: String) -> i32 {
+    let x = x.as_bytes();
+    let y = y.as_bytes();
     if y.is_empty() {
       0
     } else {
-      x.as_bytes()
-        .windows(y.len())
-        .position(|s| s == y.as_bytes())
-        .map_or(-1, |i| i as i32)
+      let i = x.windows(y.len()).position(|x| x == y);
+      i.map_or(-1, |i| i as i32)
     }
   }
 }
