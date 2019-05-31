f31fb47c8ecb16b6e0255567f5c8cc5743985318
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode/15.3-sum

--- leetcode/15.3-sum.rs
+++ leetcode/15.3-sum.rs
@@ -7,35 +7,26 @@
 // @lc code=start
 impl Solution {
     pub fn three_sum(mut xs: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut rs = vec![];
+        use std::collections::HashSet;
+        let mut rs = HashSet::new();
         xs.sort_unstable();
         for i in 0..xs.len() {
-            if i > 0 && xs[i] == xs[i - 1] {
-                continue;
-            }
             let mut i1 = i + 1;
             let mut i2 = xs.len() - 1;
             while i1 < i2 {
-                if i1 > i + 1 && xs[i1] == xs[i1 - 1] {
-                    i1 += 1;
-                    continue;
+                let r = vec![xs[i], xs[i1], xs[i2]];
+                let s = r.iter().sum::<i32>();
+                if s == 0 {
+                    rs.insert(r);
                 }
-                if i2 < xs.len() - 1 && xs[i2] == xs[i2 + 1] {
-                    i2 -= 1;
-                    continue;
-                }
-                let m = xs[i] + xs[i1] + xs[i2];
-                if m == 0 {
-                    rs.push(vec![xs[i], xs[i1], xs[i2]]);
-                }
-                if m <= 0 {
+                if s <= 0 {
                     i1 += 1;
                 } else {
                     i2 -= 1;
                 }
             }
         }
-        rs
+        rs.into_iter().collect()
     }
 }
 // @lc code=end
