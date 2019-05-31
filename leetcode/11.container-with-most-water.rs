dc99e567ef1e16ed95b7b6bf3a3e7c10dda0f35d
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode

--- leetcode/11.container-with-most-water.rs
+++ leetcode/11.container-with-most-water.rs
@@ -4,22 +4,24 @@
  * [11] Container With Most Water
  */
 
+pub struct Solution;
+
 // @lc code=start
 impl Solution {
-    pub fn max_area(xs: Vec<i32>) -> i32 {
-        let mut m = 0;
-        let mut i = 0;
-        let mut j = xs.len() - 1;
-        while i < j {
-            if (xs[i] < xs[j]) {
-                m = m.max(xs[i] * (j - i) as i32);
-                i += 1;
-            } else {
-                m = m.max(xs[j] * (j - i) as i32);
-                j -= 1;
-            }
-        }
-        m
+  pub fn max_area(xs: Vec<i32>) -> i32 {
+    let mut m = 0;
+    let mut i = 0;
+    let mut j = xs.len() - 1;
+    while i < j {
+      if xs[i] < xs[j] {
+        m = m.max(xs[i] * (j - i) as i32);
+        i += 1;
+      } else {
+        m = m.max(xs[j] * (j - i) as i32);
+        j -= 1;
+      }
     }
+    m
+  }
 }
 // @lc code=end
