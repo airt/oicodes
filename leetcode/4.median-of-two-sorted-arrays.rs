7dbfb8e8011b0f48fbc3710ad12721b6ecbd0e86
airt <airtial@gmail.com>
2019-06-01 00:00:00 +0800

    feat: leetcode/4.median-of-two-sorted-arrays

--- leetcode/4.median-of-two-sorted-arrays.rs
+++ leetcode/4.median-of-two-sorted-arrays.rs
@@ -8,12 +8,14 @@
 impl Solution {
     pub fn find_median_sorted_arrays(xs: Vec<i32>, ys: Vec<i32>) -> f64 {
         let l = xs.len() + ys.len();
-        let mut xs = xs.into_iter().peekable();
-        let mut ys = ys.into_iter().peekable();
-        let mut zs = std::iter::from_fn(|| match (xs.peek(), ys.peek()) {
-            (Some(x), yo) if yo.map_or(true, |y| x <= y) => xs.next(),
-            (xo, Some(y)) if xo.map_or(true, |x| y <= x) => ys.next(),
-            _ => None,
+        let mut xs = xs.into_iter().rev().peekable();
+        let mut ys = ys.into_iter().rev().peekable();
+        let mut zs = std::iter::from_fn(|| {
+            if xs.peek() >= ys.peek() {
+                xs.next()
+            } else {
+                ys.next()
+            }
         });
         if l % 2 == 0 {
             zs.skip(l / 2 - 1).take(2).sum::<i32>() as f64 / 2.0
